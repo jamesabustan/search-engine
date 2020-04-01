0a49d3b53493899ef656ece25e68f0b13e3d42dd
jamesabustan <jamesabustan04@gmail.com>
2020-04-01 11:42:53 +0100

    main js

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(express.static('view'));
 
 
 app.get("/search", async function (req, res){
-    let url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDrslYgDkwY3EExeY_LsNFwTImkwwikHwk&cx=016475248988972668580:dlwhrrkbeox&q=";
+    let url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDrslYgDkwY3EExeY_LsNFwTImkwwikHwk&cx=016475248988972668580:dlwhrrkbeox&q=test";
     let response = await axios.get(url);
-    res.send(response.data.items);
-   
-
+    res.send(response.data.items); 
 });
-
 
 app.listen(PORT, function() {
     console.log(`I love you ${PORT}`);
