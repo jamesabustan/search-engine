8e954c0fbcd9fb47ce1880560ef13e92bbfd043d
James Abustan <james@Jamess-MacBook-Pro.local>
2020-06-17 17:15:39 +0100

    ability to search in engine and fetch data

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ app.set("view engine", "ejs");
 // app.use(express.static('public'));
 app.use(express.static('views'));
 
-//route for home
+
+// route for homepage
+app.get('/', (req,res)=>{
+    res.render("home")
+})
+
+//route for results
 app.get("/results", (req, res) =>{
+    let qs = req.query.search;
+    let url = `https://www.googleapis.com/customsearch/v1?key=AIzaSyDrslYgDkwY3EExeY_LsNFwTImkwwikHwk&cx=016475248988972668580:dlwhrrkbeox&q=${qs}`
     async function getResults() {
         try {
-          const response = await axios.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyDrslYgDkwY3EExeY_LsNFwTImkwwikHwk&cx=016475248988972668580:dlwhrrkbeox&q=test');
+          const response = await axios.get(url);
           let results = (response.data)
           res.render("results", {results : results});
           console.log(results);
