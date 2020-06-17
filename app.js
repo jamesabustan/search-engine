1bafff7ff040b8e95a6cca9ffc06ccc6a47bfdf3
James Abustan <james@Jamess-MacBook-Pro.local>
2020-06-17 18:14:21 +0100

    style update

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const PORT = process.env.PORT || 3000;
 const axios = require('axios').default;
 
 app.set("view engine", "ejs");
-
-// app.use(express.urlencoded({extended: false}));
-// app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.static('views'));
 
 
@@ -32,19 +30,6 @@ app.get("/results", (req, res) =>{
       }
       getResults();
     });
-
-
-
-//route for search
-
-// route for results
-
-// app.get("/search", async function (req, res){
-//     let url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyDrslYgDkwY3EExeY_LsNFwTImkwwikHwk&cx=016475248988972668580:dlwhrrkbeox&q=test";
-//     let response = await axios.get(url);
-//     res.send(response.data.items); 
-// });
-
 
 
 app.listen(PORT, function() {
